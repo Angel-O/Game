1d3e4fa5e265464de68a7fc7dd9463d036287382
Angelo <angelooparah@gmail.com>
2017-04-19 06:48:06 +0100

    introduced rules to move from a place to another

--- gm.pl
+++ gm.pl
@@ -1,75 +1,108 @@
 /* <aMazeInMonkey>, by <Angelo Oparah>. */
 
+set_prolog_flag(answer_write_options,[max_depth(0)]).
+
 /* certain stuff will move around... */
 :- dynamic(i_am_at/1).
 :- dynamic(named/1). 
+:- dynamic(locked/2).
+:- dynamic(life/1).
 
 /* this section will reset the game ot the initital state when the game is reloaded */
 :- retractall(at(_, _)), retractall(i_am_at(_)), retractall(alive(_)).
 i_am_at(grey_area). /* you start the game in a neboulous place called grey_area... */
 named(player).
 
-/* this section describes how locations are connected  between them */
-path(grey_area, so, room1).
-path(grey_area, we, room1).
-path(grey_area, nr, room1).
-path(grey_area, ea, room1).
+/* ================================================================================== */
+/* this section describes which locations are connected between them and in what 
+direction */
+path(grey_area, south, room1).
+path(grey_area, west, room1).
+path(grey_area, north, room1).
+path(grey_area, east, room1).
 
-path(room1, so, grey_area).
-path(room1, we, room3).
-path(room1, nr, room2).
-path(room1, ea, room4).
+path(room1, south, grey_area).
+path(room1, west, room3).
+path(room1, north, room2).
+path(room1, east, room4).
 
-path(room3, so, room1).
-path(room3, nr, room6).
+path(room2, south, room1).
+path(room2, west, room6).
+path(room2, north, room9).
+path(room2, east, room4).
+
+path(room3, south, room1).
+path(room3, north, room6).
  
-path(room2, so, room1).
-path(room2, we, room6).
-path(room2, nr, room9).
-path(room2, ea, room4).
+path(room4, south, room1).
+path(room4, west, room2).
+path(room4, east, room8).
 
-path(room4, so, room1).
-path(room4, we, room2).
-path(room4, ea, room8).
+path(room5, south, room9).
+path(room5, west, room7).
+path(room5, north, room8).
 
-path(room6, so, room3).
-path(room6, nr, room7).
-path(room6, ea, room2).
+path(room6, south, room3).
+path(room6, north, room7).
+path(room6, east, room2).
 
-/* path(room9, so, room1). */
-path(room9, we, room5).
-path(room9, nr, room10).
-path(room9, ea, room8).
+path(room7, south, room6).
+path(room7, west, grey_area).
+path(room7, north, room10).
+/* path(room7, east, room5). */
 
-path(room8, so, room9).
-/* path(room8, we, room6). */
-path(room8, nr, jungle).
-path(room8, ea, room4).
+path(room8, south, room9).
+/* path(room8, west, room6). */
+path(room8, north, jungle).
+path(room8, east, room4).
 
-path(room7, so, room6).
-path(room7, we, grey_area).
-path(room7, nr, room10).
-/* path(room7, ea, room5). */
+/* path(room9, south, room1). */
+path(room9, west, room5).
+path(room9, north, room10).
+path(room9, east, room8).
 
-path(room5, so, room9).
-path(room5, we, room7).
-path(room5, nr, room8).
+path(room10, south, room9).
+path(room10, west, room7).
+path(room10, east, key_room).
 
-path(room10, so, room9).
-path(room10, we, room7).
-path(room10, ea, key_room).
+path(jungle, north, sroom).
 
-path(jungle, nr, sroom).
 
-/* this rule determines if two rooms are adjacent. Note how
-the comparison must come after the path rule is called
-since it is not bound before that
- */
+/* ================================================================================== */
+/* ====================== Moving around the maze ==================================== */
+
+/* Go somewhere:
+1. no longer being where I was 
+2. being somewhere new 
+3. being allowed move in sunch direction from here*/
+go(Direction) :-
+	i_am_at(Here), /* if I am Here */
+	can_go_from_here(Here, Direction), /* and the direction from the current location is clear */
+	path(Here, Direction, There), /* I will end up There if I go this Direction */
+	retract(i_am_at(Here)), /* so I will no longer be Here */
+	assert(i_am_at(There)), /* but I will be There */
+	format("You are in ~w", [There]).
+
+/* Aliases */
+n :- go(north).
+s :- go(south).
+w :- go(west).
+e : go(east).
  
-adjacent(RoomA, RoomB) :-
-	path(RoomA, _ , RoomB),
-	RoomA \= RoomB.
-	
+ can_go_from_here(Here, Direction):-
+ 	not(locked(Here, Direction)) ; /*using disjunction to evaluate second goal and print a friendly message*/
+ 	write("That door is locked."),
+ 	fail. /* since I used disjunction ONLY to evaluate the second goal I need to report a fail
+ 			otherwise the rule will be succesful */ 
+ 
+/*fix r4 r8...east..*/
+
+
+
+
+
+/* ================================================================================== */
+/* ==================== Find all paths between 2 locations ========================== */	
 /* 
 a location B can be reached from a location A if:
 1. A is adjacent to a location C, C is adjacent to a location D, 
@@ -79,12 +112,6 @@ D is adjacent to E...so on and so forth....until we get to B
 4. The final location is a location adjacent to B
 5. You are never going back to the same initial location
 */
-
-not_circular([]).
-not_circular([_|[]]).
-not_circular([H|T]):-
-	not(member(H, T)).
-
 
 /* finds all paths between two arbitrary locations using auxiliary
 function with accumulator */
@@ -112,8 +139,30 @@ all_paths_dir_aux(Start, Finish, Accumulator, Path):-
 	not(member(Next, Accumulator)),
 	append(Accumulator, [Direction, Next], NewAccumulator),
 	all_paths_dir_aux(Next, Finish, NewAccumulator, Path).
+
+/* this rule determines if two rooms are adjacent. Note how
+the comparison must come after the path rule is called
+since it is not bound before that
+ */
+%adjacent(RoomA, RoomB) :-
+%	path(RoomA, _ , RoomB),
+%	RoomA \= RoomB.
+	
+%not_circular([]).
+%not_circular([_|[]]).
+%not_circular([H|T]):-
+%	not(member(H, T)).
+	
+/* ================================================================================== */
 	
 /* start facts */
+
+locked(room8, north). /* locking the door to the jungle */
+locked(room1, north). /* to be removed.... */
+
+life(Value) :- Value = 20.
+
+win :- i_am_at(jungle). /* more to come...being alive... */
 
 first_name :-
 	write("type your name (\"in double qoutes\"): "),
@@ -122,7 +171,6 @@ first_name :-
 	assert(named(me, X)),
 	format(`welcome to aMazeInMonkey, ~w`, [X]).
 
-/* this section describes how to move */
 
 
 
